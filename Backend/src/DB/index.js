2534b6bb4329cff8c09f2ee52dba579f69eeb10a
Bhavesh Suthar <bsuthar2024@gmail.com>
2025-08-09 15:38:04 +0530

    Update index.js

--- Backend/src/DB/index.js
+++ Backend/src/DB/index.js
@@ -5,6 +5,9 @@ dotenv.config();
 
 const ConnectDB = async () => {
     try{
+        if (!process.env.MONGODB_URI) {
+            throw new Error("MONGODB_URI is missing in environment variables");
+        }
         const connection = await mongoose.connect(`${process.env.MONGODB_URI}`);
         console.log(`MongoDB successfully connected at ${connection.connection.host}`);
     }catch(err){
@@ -12,5 +15,6 @@ const ConnectDB = async () => {
         console.error(err);
     }
 }
+
 
 export default ConnectDB;
